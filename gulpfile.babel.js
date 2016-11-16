20c5d757392cbf6db4750ca9b48094e7c055cf4c
Ronen E <alter.ronen@gmail.com>
2016-11-17 00:39:59 +0200

    Add sourcemaps and bundle files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,19 @@
-import gulp from "gulp";
-import babel from "gulp-babel";
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
 
-gulp.task("default", () => {
-
-  return gulp.src("scripts/es6/*")
+gulp.task('default', () => {
+	
+	var entries = [
+		'scripts/es6/databinder.databinder.js',
+		'scripts/es6/databinder.bindings.js',
+		'scripts/es6/databinder.viewmodel.js'
+	];
+  return gulp.src(entries)
+	.pipe(sourcemaps.init())
+	.pipe(concat('bundle.js'))
 	.pipe(babel())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest("scripts/compiled"));
-
 });
